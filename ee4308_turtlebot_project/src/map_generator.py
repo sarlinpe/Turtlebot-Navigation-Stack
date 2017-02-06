88e74b603b561008af55179687d61761c4e1e8e5
prebenjensenhoel <preben_1994@hotmail.com>
2017-02-06 20:01:59 +0800

    added config file

--- ee4308_turtlebot_project/src/map_generator.py
+++ ee4308_turtlebot_project/src/map_generator.py
@@ -6,6 +6,7 @@
     http://docs.ros.org/api/nav_msgs/html/msg/Path.html
 """
 
+import config as cfg
 import rospy
 from nav_msgs.msg import OccupancyGrid, Path
 from geometry_msgs.msg import PoseStamped
@@ -17,27 +18,19 @@ class MapGenerator:
         self.pub_map = rospy.Publisher("/map", OccupancyGrid, queue_size=1, latch=True)
         self.pub_path = rospy.Publisher("/path", Path, queue_size=1, latch=True)
     
-        # TODO: get parameters from rosparam server
-        self.width = 6
-        self.height = 6
-        self.resolution = 0.1
-        self.wall_thickness = 0.2
-
         self.map = OccupancyGrid()
         self.map.header.frame_id = "1"
-        self.map.info.resolution = self.resolution
-        self.map.info.width = int(self.width/self.resolution)
-        self.map.info.height = int(self.height/self.resolution)
-        self.map.info.origin.position.x = 0.
-        self.map.info.origin.position.y = 0.
-        self.map.info.origin.position.z = 0.
+        self.map.info.resolution = cfg.RESOLUTION
+        self.map.info.width = int(cfg.WIDTH / cfg.RESOLUTION)
+        self.map.info.height = int(cfg.HEIGTH / cfg.RESOLUTION)
+        self.map.info.origin.position.x = cfg.ORIGIN_X
+        self.map.info.origin.position.y = cfg.ORIGIN_Y
+        self.map.info.origin.position.z = cfg.ORIGIN_Z
     
         self.path = Path() # May need to set a frame here...
         self.path.header.frame_id = "1"
 
-    def publish(self, walls, path):
-        offset_x = 0.5
-        offset_y = 0.5
+    def publish(self, path):
 
         # Initialize 2D map with zeros
         map = []
@@ -48,30 +41,28 @@ class MapGenerator:
             map.append(row)
 
         # Add border by setting pixels to 100
-        for i in range(self.map.info.height):
+        for i in range(self.map.info.height):                                        ##Can 100 be set as a parameter?
             map[i][0] = 100
-            map[i][self.map.info.width-1] = 100
+            map[i][self.map.info.width - 1] = 100
         for j in range(self.map.info.width):
             map[0][j] = 100
-            map[self.map.info.height-1][j] = 100
+            map[self.map.info.height - 1][j] = 100
 
         # Iterate through the walls, set the pixels accordingly
-        for (x,y) in walls:
+        for (x, y) in cfg.WALLS:
+            y += cfg.Y_OFFSET
+            x += cfg.X_OFFSET
             if (y % 1) == 0:
                 # Wall is vertical
-                x += offset_x
-                y += offset_y
-                for i in range(int(1/self.resolution)):
-                    map[int(x/self.resolution)][int((y-0.5)/self.resolution+i)] = 100
-                    map[int(x/self.resolution-1)][int((y-0.5)/self.resolution+i)] = 100
+                for i in range(int(1 / cfg.RESOLUTION)):
+                    map[int(x / cfg.RESOLUTION)][int((y - 0.5) / cfg.RESOLUTION + i)] = 100       ##(y-0.5) due to offset??? in that case cfg.Y_OFFSET
+                    map[int(x / cfg.RESOLUTION - 1)][int((y - 0.5) / cfg.RESOLUTION + i)] = 100     ##Same as above
 
             else:
                 # Wall is horizontal
-                x += offset_x
-                y += offset_y
-                for i in range(int(1/self.resolution)):
-                    map[int((x-0.5)/self.resolution+i)][int(y/self.resolution)] = 100
-                    map[int((x-0.5)/self.resolution+i)][int(y/self.resolution-1)] = 100
+                for i in range(int(1 / cfg.RESOLUTION)):
+                    map[int((x - 0.5) / cfg.RESOLUTION + i)][int(y / cfg.RESOLUTION)] = 100         ##same for (x-0.5)
+                    map[int((x - 0.5) / cfg.RESOLUTION + i)][int(y / cfg.RESOLUTION - 1)] = 100
 
         # Flatten map to self.map.data in a row-major order
         self.map.data = sum(map,[])
@@ -80,8 +71,8 @@ class MapGenerator:
         self.path.poses[:] = []
         for i in range(len(path)):
             p = PoseStamped()
-            p.pose.position.x = path[i][0] + offset_x
-            p.pose.position.y = path[i][1] + offset_y
+            p.pose.position.x = path[i][0] + cfg.X_OFFSET
+            p.pose.position.y = path[i][1] + cfg.Y_OFFSET
             p.pose.position.z = 0
             
             self.path.poses.append(p)
