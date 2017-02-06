d05a3d917eb6ac377d5f13932799f498b1b4861a
Skydes <paul-edouard.sarlin@orange.fr>
2017-02-06 11:33:26 +0800

    Updated MapGenerator

--- ee4308_turtlebot_project/src/map_generator.py
+++ ee4308_turtlebot_project/src/map_generator.py
@@ -7,7 +7,8 @@
 """
 
 import rospy
-from nav_msgs.msg import OccupancyGrid, Path
+from nav_msgs.msg import OccupancyGrid, Path, Pose
+from math import floor
 
 
 class MapGenerator:
@@ -16,8 +17,8 @@ class MapGenerator:
         self.pub_path = rospy.Publisher("/path", Path, queue_size=1)
     
         # TODO: get parameters from rosparam server
-        self.width = 9
-        self.height = 9
+        self.width = 6
+        self.height = 6
         self.resolution = 0.1
         self.wall_thickness = 0.2
 
@@ -32,6 +33,8 @@ class MapGenerator:
         self.path = Path() # May need to set a frame here...
 
     def publish(self, walls, path):
+        offset_x = 0.5
+        offset_y = 0.5
 
         # Initialize 2D map with zeros
         map = []
@@ -41,15 +44,74 @@ class MapGenerator:
                 row.append(0)
             map.append(row)
 
-        # Add border by setting pixels to 10
+        # Add border by setting pixels to 100
+        for i in range(self.map.info.height):
+            map[i][0] = 100
+            map[i][self.map.info.width-1] = 100
+        for j in range(self.map.info.width):
+            map[0][j] = 100
+            map[self.map.info.height-1][j] = 100
 
         # Iterate through the walls, set the pixels accordingly
+        for (x,y) in walls:
+            if (y % 1) == 0:
+                # Wall is vertical
+                x += offset_x
+                y += offset_y
+                for i in range(1/self.resolution):
+                    map[x/self.resoltion][(y-0.5)/self.resolution+i] = 100
+                    map[x/self.resoltion-1][(y-0.5)/self.resolution+i] = 100
+
+            else:
+                # Wall is horizontal
+                x += offset_x
+                y += offset_y
+                for i in range(1/self.resolution):
+                    map[(x-0.5)/self.resolution+i][y/self.resoltion] = 100
+                    map[(x-0.5)/self.resolution+i][y/self.resoltion-1] = 100
 
         # Flatten map to self.map.data in a row-major order
+        self.map.data = sum(map,[])
 
         # Construct Path message
+        salf.path.pose[:] = []
+        for i in range(len(path)):
+            p = Pose()
+            
+            
+            self.path.pose.append(p)
+        
 
         # Publish messages
         self.pub_map.publish(self.map)
         self.pub_path.publish(self.path)
-        
+        print map
+
+if __name__ == "__main__":
+    width   = 9
+    height  = 9
+    start   = (0,0)
+    goal    = (4,4)
+    walls = [(0.5,0),
+             (0.5,2),
+             (0.5,3),
+             (1.5,0),
+             (1.5,1),
+             (3.5,0),
+             (3.5,1),
+             (3.5,2),
+             (3.5,3),
+             (3.5,4)]
+    path = [(0,0),
+            (0,1),
+            (1,1),
+            (1,2),
+            (2,2),
+            (3,2),
+            (3,3),
+            (3,4),
+            (3,5),
+            (4,5),
+            (4,4)]
+    m = MapGenerator()
+    m.publish(walls,path)
