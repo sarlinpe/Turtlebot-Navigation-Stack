c5f2d463bab324d58c1b4f7b2a2531c058dc4fc2
Skydes <paul-edouard.sarlin@orange.fr>
2017-02-06 16:10:51 +0800

    Fixed minor bugs, added I controller, tuned controller parameters

--- ee4308_turtlebot_project/src/local_planner.py
+++ ee4308_turtlebot_project/src/local_planner.py
@@ -10,21 +10,22 @@
 """
 
 import rospy
-from import math import atan2, sqrt, pow, pi
+from math import atan2, sqrt, pow, pi
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import Twist, Vector3
-from tf.transformation import euler_from_quaternion
+from tf.transformations import euler_from_quaternion
 
 from global_planner import AStar as pathSearch, globalSmoothing
 from map_generator import MapGenerator
 
 # MAP PARAMETERS
-local_smoothing = True
+local_smoothing = False
+global_smoothing = True
 known_map = True
 width   = 9
 height  = 9
 start   = (0,0)
-goal    = (4,4)
+goal    = (4,3)
 walls = [(0.5,0),
          (0.5,2),
          (0.5,3),
@@ -33,20 +34,19 @@ walls = [(0.5,0),
          (3.5,0),
          (3.5,1),
          (3.5,2),
-         (3.5,3),
-         (3.5,4)]
+         (3.5,3)]
 
 # CONTROL PARAMETERS
-TOL_ORIENT  = 0.001
+TOL_ORIENT  = 0.01
 TOL_DIST    = 0.1
-K_p_orient	= 1
-K_p_dist	= 0.1
+K_p_orient	= 0.8
+K_p_dist	= 0.6
 K_i_orient	= 5e-4
-K_i_dist	= 2e-5
+K_i_dist	= 1e-3
 
 # SMOOTHING PARAMETERS
-SMOOTH_NB_PTS   = 2
-SMOOTH_WEIGHTS  = [3, 1]
+SMOOTH_NB_PTS   = 4
+SMOOTH_WEIGHTS  = [4, 3, 1, 1]
 
 class CtrlStates:
     Orient, Move, Wait = range(3)
@@ -56,13 +56,13 @@ class LocalPlanner:
 
     def __init__(self):
         rospy.Subscriber("/odom_true", Odometry, self.makePlan)
-        self.pub = rospy.Publisher("/cmd_vel_mux/input/teleop", Odometry, queue_size=1)
+        self.pub = rospy.Publisher("/cmd_vel_mux/input/teleop", Twist, queue_size=1)
         self.gen = MapGenerator()
         
-        # TODO: check exception is no path is found
+        # TODO: check exception if no path is found
         self.path = pathSearch(start, goal, walls, width, height)
-        if not local_smoothing:
-            self.path = globalSmoothing(self.path, 0.7)
+        if global_smoothing:
+            self.path = globalSmoothing(self.path, 0.2)
         self.pts_cnt = 0
         self.ctrl_state = CtrlStates.Orient
         
@@ -72,11 +72,16 @@ class LocalPlanner:
 
     def makePlan(self, odom):
         # Extract relevant state variable from Odometry message
-        euler = euler_from_quaternion(odom.pose.pose.orientation)
+        quaternion = (
+            odom.pose.pose.orientation.x,
+            odom.pose.pose.orientation.y,
+            odom.pose.pose.orientation.z,
+            odom.pose.pose.orientation.w)
+        euler = euler_from_quaternion(quaternion)
         theta = euler[2]
         pos_x = odom.pose.pose.position.x - 0.5
         pos_y = odom.pose.pose.position.y - 0.5
-
+        
         v_lin = 0.
         v_ang = 0.
         
@@ -86,7 +91,7 @@ class LocalPlanner:
             (x,y) = self.path[self.pts_cnt]
             goal_theta = atan2(y-pos_y,x-pos_x)
             err_theta = self.checkAngle(goal_theta - theta)
-    
+            
             # Check if satisfactory => start motion
             if err_theta < TOL_ORIENT :
                 self.ctrl_state = CtrlStates.Move
@@ -94,7 +99,7 @@ class LocalPlanner:
                 self.sum_dist = 0.
             # Else adjust orientation
             else:
-                sum_theta += err_theta
+                self.sum_theta += err_theta
                 v_ang = K_p_orient*err_theta + K_i_orient*self.sum_theta
         
         # Control motion between waypoints
@@ -112,7 +117,7 @@ class LocalPlanner:
                 err_theta_raw = atan2(y-pos_y,x-pos_x) - theta
                 err_theta.append(self.checkAngle(err_theta_raw))
                 err_dist.append(sqrt(pow(x-pos_x,2)+pow(y-pos_y,2)))
-        
+            
             # TODO:
             # - check if any point is under the tolerance -> shift pts_cnt
             # - computer v_lin and v_ang with a defined PI control law
@@ -132,28 +137,28 @@ class LocalPlanner:
                     err_theta_tot = 0
                     err_theta_scaling = 0
                     err_dist_tot = 0
-                    err_dits_scaling = 0
+                    err_dist_scaling = 0
                     for i in range(len(err_theta)):
                         err_theta_tot += err_theta[i]*err_dist[i]*SMOOTH_WEIGHTS[i]
                         err_theta_scaling += err_dist[i]*SMOOTH_WEIGHTS[i]
                         err_dist_tot += err_dist[i]*SMOOTH_WEIGHTS[i]
-                        err_dits_scaling += SMOOTH_WEIGHTS[i]
+                        err_dist_scaling += SMOOTH_WEIGHTS[i]
                     err_theta_tot /= err_theta_scaling
                     err_dist_tot /= err_dist_scaling
+                    self.sum_theta += err_theta_tot
+                    self.sum_dist += err_dist_tot
                 else:
                     err_theta_tot = err_theta[0]
                     err_dist_tot = err_dist[0]
-                    
-                    #sum_theta += err_theta[0]
-                    #sum_dist += err_dist[0]
+                    self.sum_theta += err_theta[0]
+                    self.sum_dist += err_dist[0]
                 
-                vel_z = K_p_orient*err_theta_tot    #+ K_i_orient*sum_theta
-                vel_x = K_p_dist*err_dist_tot       #+ K_i_dist*sum_dist
+                v_ang = K_p_orient*err_theta_tot    + K_i_orient*self.sum_theta
+                v_lin = K_p_dist*err_dist_tot       + K_i_dist*self.sum_dist
         
         # Wait for a new goal point
         else:
             pass
-        
         cmd = Twist()
         cmd.linear.x = v_lin
         cmd.angular.z = v_ang
@@ -172,7 +177,7 @@ class LocalPlanner:
 if __name__ == "__main__":
     rospy.init_node("local_planner", anonymous=True)
     try:
-        node = Local_planner()
+        node = LocalPlanner()
         rospy.spin()
     except rospy.ROSInterruptException:
         rospy.loginfo("Shutting down node: %s", rospy.get_name())
