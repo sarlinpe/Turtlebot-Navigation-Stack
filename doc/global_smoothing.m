528b22f6af96c7dcca52390b323b42b7cd79e2a3
Skydes <paul-edouard.sarlin@orange.fr>
2017-03-01 23:08:29 +0800

    Improved output of the global planning Matlab script

--- doc/global_smoothing.m
+++ doc/global_smoothing.m
@@ -5,7 +5,8 @@ path = [0,0;
         1,1;
         1,2;
         2,2;
-        2,3;
+        2,3;%
+        %3,2;%
         3,3;
         3,4;
         3,5;
@@ -22,11 +23,11 @@ walls = [0.5,0;
          3.5,3;
          3.5,4];
     
-alpha = 0.2;
+alpha = 0.2; %0.7
 rate = 1;
 tol = 1e-6;
 
-density = 4;
+density = 4; %1
 dense = [];
 for i = 1:(length(path)-1)
     for j = 0:(density-1)
@@ -61,6 +62,19 @@ for i = 1:size(walls,1)
         line([walls(i,1),walls(i,1)], [walls(i,2)-0.5, walls(i,2)+0.5],...
             'Color','k','LineWidth',4);
     else
-        
+        line([walls(i,1)-0.5,walls(i,1)+0.5], [walls(i,2), walls(i,2)],...
+            'Color','k','LineWidth',4);
     end
+end
+
+axis equal
+axis([-0.5 4.5 -0.5 5.5])
+xticks(-1:4)
+
+x = -0.5:1:4.5; y = -0.5:1:5.5;
+xv = repmat(x',1,2); yv = repmat([y(1),y(end)],length(x),1);
+xh = repmat([x(1),x(end)],length(y),1); yh = repmat(y',1,2);
+for i = 1:length(xv)
+    line(xv(i,:),yv(i,:),'Color',[0.5,0.5,0.5]);
+    line(xh(i,:),yh(i,:),'Color',[0.5,0.5,0.5]);
 end
